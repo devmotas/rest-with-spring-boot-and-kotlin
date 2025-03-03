564d3a5bd92745a60b0d899c874862441ccc8a9a
Gustavo Mota <79374856+devmotas@users.noreply.github.com>
2025-03-03 14:52:29 -0300

    Add sum request

---
+++ rest-with-spring-boot-and-kotlin/src/main/kotlin/com/example/exceptions/CustomizedResponseEntityExceptionHandler.kt
@@ -0,0 +1,36 @@
+package com.example.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import java.util.*
+
+@ControllerAdvice
+@RestController
+class CustomizedResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    fun handlerAllExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            Date(), ex.message,
+            request.getDescription(false),
+        )
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException::class)
+    fun handlerBadRequestExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            Date(), ex.message,
+            request.getDescription(false),
+        )
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+}
---
+++ rest-with-spring-boot-and-kotlin/src/main/kotlin/com/example/exceptions/UnsupportedMathOperationException.kt
@@ -0,0 +1,5 @@
+package com.example.exceptions
+
+class UnsupportedMathOperationException(exception: String?) : RuntimeException(exception) {
+
+}
